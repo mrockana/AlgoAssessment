a831d8867f27f0da005bae2285b2cd2fd4172acb
Mpho Molelle <mrockana@gmail.com>
2021-03-01 08:10:46 +0200

    implemented Anagram

--- TGS.Challenge/Anagram.cs
+++ TGS.Challenge/Anagram.cs
@@ -1,30 +1,77 @@
+using System;
+using System.Collections.Generic;
+
 namespace TGS.Challenge
 {
-  /*
-        Devise a function that checks if 1 word is an anagram of another, if the words are anagrams of
-        one another return true, else return false
+    /*
+          Devise a function that checks if 1 word is an anagram of another, if the words are anagrams of
+          one another return true, else return false
 
-        "Anagram": An anagram is a type of word play, the result of rearranging the letters of a word or
-        phrase to produce a new word or phrase, using all the original letters exactly once; for example
-        orchestra can be rearranged into carthorse.
+          "Anagram": An anagram is a type of word play, the result of rearranging the letters of a word or
+          phrase to produce a new word or phrase, using all the original letters exactly once; for example
+          orchestra can be rearranged into carthorse.
 
-        areAnagrams("horse", "shore") should return true
-        areAnagrams("horse", "short") should return false
+          areAnagrams("horse", "shore") should return true
+          areAnagrams("horse", "short") should return false
 
-        NOTE: Punctuation, including spaces should be ignored, e.g.
+          NOTE: Punctuation, including spaces should be ignored, e.g.
 
-        horse!! shore = true
-        horse  !! shore = true
-          horse? heroes = true
+          horse!! shore = true
+          horse  !! shore = true
+            horse? heroes = true
 
-        There are accompanying unit tests for this exercise, ensure all tests pass & make
-        sure the unit tests are correct too.
-     */
+          There are accompanying unit tests for this exercise, ensure all tests pass & make
+          sure the unit tests are correct too.
+       */
+
     public class Anagram
     {
-      public bool AreAnagrams(string word1, string word2)
-      {
-        return false;
-      }
+        public bool AreAnagrams(string word1, string word2)
+        {
+            if (string.IsNullOrEmpty(word1))
+            {
+                throw new ArgumentException($"{nameof(word1)} cannot be empty.");
+            }
+
+            if (string.IsNullOrEmpty(word2))
+            {
+                throw new ArgumentException($"{nameof(word2)} cannot be empty.");
+            }
+
+            word1 = word1.ToUpperInvariant();
+            word2 = word2.ToUpperInvariant();
+
+            var word1Array = RemovePantuationsOrSpace(word1.ToCharArray());
+            var word2Array = RemovePantuationsOrSpace(word2.ToCharArray());
+
+            if (word1Array.Length != word2Array.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < word1Array.Length; i++)
+            {
+                if (!Array.Exists(word2Array, e => e == word1Array[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private char[] RemovePantuationsOrSpace(char[] array)
+        {
+            var resultList = new List<char>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!char.IsPunctuation(array[i]) && !char.IsWhiteSpace(array[i]))
+                {
+                    resultList.Add(array[i]);
+                }
+            }
+
+            return resultList.ToArray();
+        }
     }
 }
